f558a80a5c5e7d30ed341efbc3251b88354e9a6e
unknown <siu1@hi.is>
2019-11-27 15:57:14 +0000

    small start

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -3,3 +3,19 @@ export function empty(element) {
     element.removeChild(element.firstChild);
   }
 }
+
+export function el(name, ...children) {
+  const element = document.createElement(name);
+
+  if (Array.isArray(children)) {
+    children.forEach((child) => {
+      if (typeof child === 'string') {
+        element.appendChild(document.createTextNode(child));
+      } else if (child) {
+        element.appendChild(child);
+      }
+    });
+  }
+
+  return element;
+}
