2819a53ae764567febf87f79fd63715f2f583dce
unknown <siu1@hi.is>
2019-11-28 03:44:11 +0000

    late night cant sleep

--- src/lib/lectures.js
+++ src/lib/lectures.js
@@ -1,7 +1,10 @@
-import { el, empty } from './helpers';
+/*import { el, empty } from './helpers';
 
 const ourJson = json.parse('/lectures.json');
 
 export default function init() {
-    
-} 
+    const mainDocument = document.querySelector('main');
+    ourJson.forEach((obj) => {
+        const q;
+    })
+} */
