4dcb5a23d53abf22ee0ff31463811fd4f3d7166e
unknown <siu1@hi.is>
2019-11-29 21:14:45 +0000

    header rdy?

--- src/lib/lectures.js
+++ src/lib/lectures.js
@@ -1,10 +1,11 @@
-import { el, empty, arrayRemove, arrayAdd } from './helpers';
+import { el, empty, arrayRemove, arrayAdd, loadStorage, save, clear } from './helpers';
 
 let selected = [];
 
 let htmlButton;
 let cssButton;
 let jsButton;
+let finished = 'Klára Fyrirlestur';
 
 export default function init() {
     htmlButton = document.querySelector('#html-button');
@@ -51,16 +52,14 @@ export async function lectureJSON() {
 function load(data) {
     const mainDocument = document.querySelector('.lecturebox');
     empty(mainDocument);
-    console.log(selected);
     data.forEach((obj) => {
         if(!selected.length || selected.includes(obj.category)) {
             const ourThumbnail = document.createElement('img');
             if(obj.hasOwnProperty("thumbnail")) {
               ourThumbnail.src = obj.thumbnail;
-              ourThumbnail.classList.add('lecturebox__image');
-            } else {
-              ourThumbnail.classList.add('lecturebox__fakeImage');  
             }
+            
+            ourThumbnail.classList.add('lecturebox__image');
 
             const firstHeading = document.createElement('div');
             firstHeading.classList.add('heading', 'heading__size1');
@@ -68,7 +67,6 @@ function load(data) {
 
             const secondHeading = document.createElement('div');
             secondHeading.classList.add('heading', 'heading__size2');
-            //const secondHeadingText = document.createElement('p');
             secondHeading.textContent = obj.title;
 
             const ourCard = el('a', ourThumbnail, firstHeading, secondHeading);
@@ -120,10 +118,12 @@ export function readLecture(data) {
     headCategory.classList.add("heading__size1");
     head.appendChild(headCategory);
 
+    const headTitle = el('h1', data.title);
+    head.appendChild(headTitle);
+
     const page = document.querySelector(".lecture-page");
 
     data.content.forEach((obj) => {
-        console.log(obj.type);
         if(obj.type === "quote"){
           const quoteData = document.createElement('div');
           quoteData.classList.add("lecture-page__quote");
@@ -156,11 +156,10 @@ export function readLecture(data) {
                 listData.appendChild(listText);
             }
             page.appendChild(listData);
-        } else if(obj.type === 'text') {//linebreak
+        } else if(obj.type === 'text') {
             const txtData = document.createElement('p');
             txtData.classList.add('lecture-page__text');
             txtData.textContent = (obj.data).replace('\\n','<br><br>');
-            console.log(txtData.textContent);
             page.appendChild(txtData);
         } else if(obj.type === "heading"){
             const headingData = document.createElement('h2');
@@ -172,7 +171,6 @@ export function readLecture(data) {
             const ytData = document.createElement('iframe');
             ytFrame.classList.add('lecture-page__youtube');
             ytData.src = obj.data;
-            console.log(ytData.src);
             ytData.frameborder = '0';
             ytData.allowfullscreen = '0';
             ytFrame.appendChild(ytData);
@@ -183,7 +181,6 @@ export function readLecture(data) {
             ourImg.classList.add('lecture-page__image');
             imgData.src = '../'.concat(obj.data);
             ourImg.appendChild(imgData);
-            //ehv til að sja hvaða mynd
             if(obj.hasOwnProperty('caption')){
                 const imgCaption = document.createElement('p');
                 imgCaption.textContent = obj.caption;
@@ -192,4 +189,29 @@ export function readLecture(data) {
             page.appendChild(ourImg);
         }
     });
+
+    const finishLecture = document.createElement('div');
+    finishLecture.classList.add('lecture-page__link');
+    let ourStorage = loadStorage();
+    console.log(ourStorage);
+    if(ourStorage != null && ourStorage.some((obj) => obj.title === data.title)) {
+      finishLecture.textContent = '✓ Fyrirlestur Kláraður';
+      finishLecture.style.color = '#2d2';
+    } else {
+      finishLecture.textContent = 'Klára fyrirlestur';
+      finishLecture.addEventListener('click', function(){finishedLecture(data.title, finishLecture)});
+    }
+
+    const backLink = document.createElement('a');
+    backLink.textContent = 'Til Baka';
+    backLink.href = 'index.html';
+    backLink.classList.add('lecture-page__link');
+    page.appendChild(finishLecture);
+    page.appendChild(backLink);
+}
+
+function finishedLecture(title, node) {
+    save(title)
+    node.textContent = '✓ Fyrirlestur Kláraður';
+    node.style.color = '#2d2';
 }
